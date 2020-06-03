a28882cd533f0d84568bca622d7ce4b256479120
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-06-03 10:45:37 +0530

    Create ALEXNUMB.cpp

---
+++ ALEXNUMB.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--)
+{
+    long n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        long x;
+        cin>>x;
+    }
+    cout<<n*(n-1)/2<<endl;
+    }// your code goes here
+	return 0;
+}
